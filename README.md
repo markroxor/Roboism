e1f2ed15b165b132d4ba5f7f5a95321fdaa8f23c
markroxor <mrmohitrathoremr@gmail.com>
2016-06-18 12:26:39 +0530

    dynamic url in profile fix

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 [![Build Status](https://travis-ci.org/markroxor/Roboism.svg?branch=master)](https://travis-ci.org/markroxor/Roboism)
 [![Dependency status](https://gemnasium.com/markroxor/Roboism.svg)](https://gemnasium.com/markroxor/Roboism)
+
 # RoboISM - Running at roboism.pythonanywhere.com
 
-Issues to be sorted out -> 
+Issues to be sorted out -
 
-1. Go to templates -> active-members.html, there find div class="W3-accordion". Problem is that individual accordion for bio of each member cannot be opened. If clicked, only opens first bio. Please sort it out as soon as possible. 
+* Go to templates -> active-members.html, there find div class="W3-accordion". Problem is that individual accordion for bio of each member cannot be opened. If clicked, only opens first bio. Please sort it out as soon as possible.
 
-2. Profile pic of member's is not showing due to dynamic url in img src="{{ ? }}" is not correct. Please sort it out as soon as possible. Same problem with resume download. 
-
-
+* ~~Profile pic of member's is not showing due to dynamic url in img src="{{ ? }}" is not correct. Please sort it out as soon as possible. Same problem with resume download.~~
--- mainsite/models.py
+++ mainsite/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.utils import timezone
+import os
 
+
+from django import template
+register = template.Library()
 
 class Member(models.Model):
-    pic = models.ImageField()
+    pic = models.ImageField(upload_to='./mainsite/static/')
     username = models.CharField(default="Username", max_length=20)
     password = models.CharField(default="Password" ,max_length=20)
     name = models.CharField(default="Name", max_length=50)
@@ -14,14 +18,26 @@ class Member(models.Model):
     year = models.CharField(default="Year", max_length=12)
     bio = models.TextField(default="Lorem ipsum sit dolot fuck this shit !")
     linkedin = models.URLField(null=True)
-    resume = models.FileField(null=True)
+    resume = models.FileField(null=True,upload_to='./mainsite/static')
     active = models.BooleanField()
+
+    # resume.name="pop"
+    # os.path.basename(resume.name)
 
     def publish(self):
         self.save()
 
     def __str__(self):
         return self.name
+
+
+    @property
+    def resumeFilename(self,):
+        return  os.path.basename(self.resume.name)
+
+    @property
+    def picFilename(self,):
+        return  os.path.basename(self.pic.name)
 
 
 class Project(models.Model):
