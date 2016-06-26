3b246afda202befb5c124295dc297d9cbdb07740
devojoyti <devjyoti.itachi@gmail.com>
2016-06-27 04:18:16 +0530

    added roboexpo

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Member, Project
+from .models import *
 
 admin.site.register(Member)
 admin.site.register(Project)
+admin.site.register(ExpoProject)
